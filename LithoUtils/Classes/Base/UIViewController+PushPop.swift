2543749622ba236e58ea4f2105e525f2af815969
Calvin Collins <cjc8@williams.edu>
2021-05-17 13:00:24 -0700

    added combine

--- LithoUtils/Classes/Base/UIViewController+PushPop.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UIViewController+PushPop.swift
-//  FunNet
-//
-//  Created by Calvin Collins on 5/17/21.
-//
-
-import Foundation
---
+++ LithoUtils/Classes/Combine/BindViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  BindViewModel.swift
+//  FunNet
+//
+//  Created by Calvin Collins on 5/17/21.
+//
+
+import Foundation
+import Combine
+import LithoOperators
+import Prelude
+
+public func bindButtonEnabledToPublisher(_ button: UIButton, publisher: AnyPublisher<Bool, Never>, cancelBag: inout Set<AnyCancellable>){
+    publisher.sink{ button.isEnabled = $0 }.store(in: &cancelBag)
+}
+
+public func bindActivityIndicatorVisibleToPublisher(_ activity: UIActivityIndicatorView, publisher: AnyPublisher<Bool, Never>, cancelBag: inout Set<AnyCancellable>) {
+    publisher.sink{ activity.isHidden = !$0 }.store(in: &cancelBag)
+}
+
+extension Publisher where Output == Bool, Failure == Never {
+    public func bind<T>(to keyPath: WritableKeyPath<T, Bool>, on value: T, storingIn cancelBag: inout Set<AnyCancellable>) {
+        self.sink(receiveValue: value >|> setter(keyPath)).store(in: &cancelBag)
+    }
+}
