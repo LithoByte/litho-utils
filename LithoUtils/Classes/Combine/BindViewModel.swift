460a528111ea34fccdccc1d32f083dfbb569099c
Calvin Collins <cjc8@williams.edu>
2021-05-17 14:12:07 -0700

    added bind view model

--- LithoUtils/Classes/Combine/BindViewModel.swift
+++ LithoUtils/Classes/Combine/BindViewModel.swift
@@ -10,14 +10,17 @@ import Combine
 import LithoOperators
 import Prelude
 
+@available(iOS 13.0, *)
 public func bindButtonEnabledToPublisher(_ button: UIButton, publisher: AnyPublisher<Bool, Never>, cancelBag: inout Set<AnyCancellable>){
     publisher.sink{ button.isEnabled = $0 }.store(in: &cancelBag)
 }
 
+@available(iOS 13.0, *)
 public func bindActivityIndicatorVisibleToPublisher(_ activity: UIActivityIndicatorView, publisher: AnyPublisher<Bool, Never>, cancelBag: inout Set<AnyCancellable>) {
     publisher.sink{ activity.isHidden = !$0 }.store(in: &cancelBag)
 }
 
+@available(iOS 13.0, *)
 extension Publisher where Output == Bool, Failure == Never {
     public func bind<T>(to keyPath: WritableKeyPath<T, Bool>, on value: T, storingIn cancelBag: inout Set<AnyCancellable>) {
         self.sink(receiveValue: value >|> setter(keyPath)).store(in: &cancelBag)
