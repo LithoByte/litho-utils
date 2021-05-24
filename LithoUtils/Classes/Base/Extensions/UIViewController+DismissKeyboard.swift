44f437092ebc3eefead35d36939c0d28696bd6af
Calvin Collins <cjc8@williams.edu>
2021-05-24 12:11:29 -0700

    more stuff from lux

---
+++ LithoUtils/Classes/Base/Extensions/UIViewController+DismissKeyboard.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+DismissKeyboard.swift
+//  LithoUtils
+//
+//  Created by Calvin Collins on 5/24/21.
+//
+
+import UIKit
+
+public extension UIViewController {
+    @objc func dismissKeyboard() { view.endEditing(true) }
+    func addTapToDismissKeyboard() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+}
+public func addTapToDismissKeyboard(_ vc: UIViewController) {
+    vc.addTapToDismissKeyboard()
+}
---
+++ LithoUtils/Classes/Base/Extensions/UIWindow+Present.swift
@@ -0,0 +1,28 @@
+//
+//  UIWindow+Present.swift
+//  LithoUtils
+//
+//  Created by Calvin Collins on 5/24/21.
+//
+
+import Foundation
+
+public extension UIWindow {
+    func present<T>(_ vc: T) where T: UIViewController {
+        if let rootViewController = rootViewController {
+            if let navVC = rootViewController as? UINavigationController, let visibleVC = navVC.visibleViewController {
+                visibleVC.present(vc, animated: true, completion: nil)
+            } else if let tabVC = rootViewController as? UITabBarController, let selectedVC = tabVC.selectedViewController {
+                selectedVC.present(vc, animated: true, completion: nil)
+            } else {
+                rootViewController.present(vc, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func presentClosure() -> (UIViewController) -> Void {
+        return { [weak self] vc in
+            self?.present(vc)
+        }
+    }
+}
