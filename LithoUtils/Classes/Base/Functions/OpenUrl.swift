682b6f2ff14cd2a95c55b89ea4b1e77f62010516
Calvin Collins <calvin.john.collins@gmail.com>
2021-11-30 15:32:27 -0800

    changed to openUrl

--- LithoUtils/Classes/Base/Functions/OpenUrl.swift
+++ LithoUtils/Classes/Base/Functions/OpenUrl.swift
@@ -9,4 +9,4 @@ import Foundation
 import LithoOperators
 import UIKit
 
-public let openURL = URL.init(string:) >?> (([:], nil) -**> UIApplication.shared.open)
+public let openUrl = URL.init(string:) >?> (([:], nil) -**> UIApplication.shared.open)
