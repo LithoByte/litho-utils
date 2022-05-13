8d2923d7854bae734970c929f31619530999e225
Calvin Collins <calvin.john.collins@gmail.com>
2022-05-13 14:27:05 -0400

    added tappable views

---
+++ LithoUtils/Classes/Base/Models/TappableView.swift
@@ -0,0 +1,59 @@
+//
+//  TappableView.swift
+//  LithoUtils
+//
+//  Created by Calvin Collins on 5/13/22.
+//
+
+import Foundation
+import LithoOperators
+
+public class TappableView: UIView {
+    public var onTap: ((TappableView) -> Void)?
+    
+    public convenience init(frame: CGRect, onTap: @escaping (TappableView) -> Void) {
+        self.init(frame: frame)
+        self.onTap = onTap
+    }
+    
+    public convenience init?(coder: NSCoder, onTap: @escaping (TappableView) -> Void) {
+        self.init(coder: coder)
+        self.onTap = onTap
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupGestureRecognizers()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupGestureRecognizers()
+    }
+    
+    private func setupGestureRecognizers() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
+        addGestureRecognizer(tap)
+    }
+    
+    @objc func viewTapped() {
+        onTap?(self)
+    }
+}
+
+public class TappableModelView<T>: TappableView {
+    public let model: T?
+    public init(frame: CGRect, model: T, onTap: @escaping (T, TappableView) -> Void) {
+        self.model = model
+        super.init(frame: frame, onTap: model *-> onTap)
+    }
+    
+    public init?(coder: NSCoder, model: T, onTap: @escaping (T, TappableView) -> Void) {
+        self.model = model
+        super.init(coder: coder, onTap: model *-> onTap)
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
